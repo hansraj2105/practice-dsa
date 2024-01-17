7a7d686fcabb50d3d055f2c44a4f6c0475ae53b7
Hansraj Maurya <hansrajmaurya.edunext@gmail.com>
2024-01-17 15:18:17 +0530

    loop

--- src/main/java/org/example/PrintSubSting.java
+++ src/main/java/org/example/PrintSubSting.java
@@ -13,8 +13,18 @@ public class PrintSubSting {
         }
         pintStr(str.substring(1));
     }
+    public static void pintStrInloop(String str){
+        for (int i=0;i<str.length();i++){
+        int j=i;
+        while (str.length() > j){
+            j++;
+           String newString= str.substring(i,j);
+            System.out.println(newString);
+        }
+        }
+    }
     public static void main(String[] args) {
         String str = "abcd";
-        pintStr(str);
+        pintStrInloop(str);
     }
 }
